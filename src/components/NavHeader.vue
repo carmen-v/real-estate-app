abc75e49f968acd3f0b30df6408043c6da57bd00
carmen.vernica <carmen.vernica@scouting.nl>
2024-04-06 12:11:16 +0200

    us-1: add nav header

---
+++ src/components/NavHeader.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <nav class="container">
+      <div class="branding">
+        <img src="@/assets/images/img_logo_dtt@3x.png" alt="logo" />
+      </div>
+      <ul class="nav-routes">
+        <RouterLink to="/">Houses</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" scoped></style>
